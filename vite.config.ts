9d52aabcff52cc4999af99ad25cf28df36a21db6
msanchez-jusan <msanchez@jusan.com.es>
2026-07-09 14:28:08 +0200

    Cambios Para hacerla aplicacion PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,37 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    VitePWA({
+      registerType: "autoUpdate", // Actualiza la app automáticamente cuando subas cambios
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Gym Tracker System",
+        short_name: "Tracker",
+        description: "Tracker histórico de entrenamiento con IA local",
+        theme_color: "#000000", // Fondo negro para la barra de carga
+        background_color: "#000000", // Fondo negro
+        display: "standalone", // 👈 CLAVE: Oculta la barra de Safari
+        orientation: "portrait",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+});
